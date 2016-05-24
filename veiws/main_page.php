4665ae0750c43fe1bf24c5340ea8144f77fe88b3
zooboss <sandrozz@yandex.ru>
2016-05-25 02:10:24 +0300

    добавлен viewport и custom.css

--- veiws/main_page.php
+++ veiws/main_page.php
@@ -1,8 +1,11 @@
 <!DOCTYPE HMTL>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Psychology site</title>
     <link rel="stylesheet" href="libraries/css/bootstrap.css">
+    <link rel="stylesheet" href="custom.css">
 </head>
 <body>
     <h1>Hello, world!</h1>
