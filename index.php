62db0699be281aefe12ff205e810c27b24974fca
zooboss <sandrozz@yandex.ru>
2016-05-24 22:31:19 +0300

    каркас для психологического сайта

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once(dirname(__FILE__) . "/models/database.php");
+require_once(dirname(__FILE__) . "/models/database.php");
+
+include(dirname(__FILE__) . "/veiws/main_page.php");
---
+++ veiws/main_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HMTL>
+<html>
+<head>
+    <title>Psychology site</title>
+    
+</head>
+<body>
+    <h1>Hello, world!</h1>
+    
+    <?php 
+        $link = db_connect();
+        var_dump($link); 
+    ?>
+    
+    
+    
+    
+    
+    
+    
+</body>
+</html>
