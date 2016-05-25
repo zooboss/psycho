10309d4b28df3b4ea96ac39781eac2a5d64da6e1
zooboss <sandrozz@yandex.ru>
2016-05-25 08:15:55 +0300

    добавлен верхний лэйаут и его техническая раскрасска

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once(dirname(__FILE__) . "/models/database.php");
-require_once(dirname(__FILE__) . "/models/database.php");
+require_once(dirname(__FILE__) . "/models/functions.php");
+
+$link = db_connect();
 
 include(dirname(__FILE__) . "/veiws/main_page.php");
--- veiws/main_page.php
+++ veiws/main_page.php
@@ -5,21 +5,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Psychology site</title>
     <link rel="stylesheet" href="libraries/css/bootstrap.css">
-    <link rel="stylesheet" href="custom.css">
+    <link rel="stylesheet" href="css/custom.css">
 </head>
 <body>
-    <h1>Hello, world!</h1>
     
-    <?php 
-        $link = db_connect();
-        var_dump($link); 
-    ?>
+    <header class = "container">
+        <div class = "row">
+            <div class = "col-md-12">
+                <h1>HEADER</h1>
+            </div>
+        </div>
+    </header>
     
+    <section class = "container">
+        <div class = "row">
+            <div class = "col-md-12">
+                <h1>INTRO</h1>
+            </div>
+        </div>
+        <div class = "row">
+            <div class = "col-lg-6 col-md-6 col-sm-6">
+                <h1>INFO NASTYA</h1>
+            </div>
+            <div class = "col-lg-6 col-md-6 col-sm-6">
+                <h1>INFO LUDA</h1>
+            </div>
+        </div>
+    </section>
     
+    <section class = "container">
+        <div class = "row">
+            <div class = "col-md-12">
+                <h1>HELP DIRECTIONS</h1>
+            </div>
+        </div>
+    </section>
     
+    <footer class = "container">
+        <div class = "row">
+            <div class = "col-lg-4 col-md-4 col-sm-4">
+                <h1>LIST</h1>
+            </div>
+            <div class = "col-lg-4 col-md-4 col-sm-4">
+                <h1>CONTACTS</h1>
+            </div>
+            <div class = "col-lg-4 col-md-4 col-sm-4">
+                <h1>NAME AND COPYRIGHT</h1>
+            </div>
+        
+        </div>
     
-    
-    
-    
+    </footer>
+        
 </body>
 </html>
