3db2c86198e8376863f83b780d96076b44100e7a
zooboss <sandrozz@yandex.ru>
2016-05-26 23:48:03 +0300

    внедрены два меню (модальные окна для контактов и обратной связи),  так же создана сетка для направлений помощи

--- veiws/main_page.php
+++ veiws/main_page.php
@@ -5,14 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Psychology site</title>
     <link rel="stylesheet" href="libraries/css/bootstrap.css">
+    <script rel = "text/javascript" src = "libraries/js/jquery-2.2.4.js"></script>
+    <script rel="text/javascript" src="libraries/js/bootstrap.js"></script>
     <link rel="stylesheet" href="css/custom.css">
 </head>
 <body>
     
     <header class = "container">
         <div class = "row">
-            <div class = "col-md-12">
-                <h1>HEADER</h1>
+            <div class = "col-md-3 col-sm-3 col-xs-3">
+                <h4>Психологический центр</h4>
+            </div>
+            <div class = "col-md-6 col-sm-3 col-xs-3">
+                <h1>Название</h1>
+            </div>
+            <div class = "col-md-3 col-sm-3 col-xs-3">
+            
             </div>
         </div>
     </header>
@@ -24,19 +32,225 @@
             </div>
         </div>
         <div class = "row">
-            <div class = "col-lg-6 col-md-6 col-sm-6">
-                <h1>INFO NASTYA</h1>
+            <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <img src = "images/nastya_smooth.jpg" class = "img-responsive img-circle center-block">
+                <p class = "lead text-justify">
+                    информационный текст текст текст текст
+                    текст текст. текст етксткт. текст текст.
+                    текст етксткт. текст текст. текст етксткт.
+                    текст етксткт. текст текст.
+                </p>
+                <a href = "views/anastasiya" role = "button" class = "btn btn-block btn-primary btn-lg">Блог</a>
+                <!-- MODAL TRIGGER FOR NASTYA'S CONTACTS -->
+                <button type = "button" class = "btn btn-block btn-primary btn-lg" data-toggle = "modal" data-target = "#contacts_nastya">контакты</button>
+                <!-- MODAL TRIGGER FOR MAILTO -->
+                <button type = "button" class = "btn btn-block btn-success btn-lg" data-toggle = "modal" data-target = "#mailto">оставить сообщение</button>
             </div>
-            <div class = "col-lg-6 col-md-6 col-sm-6">
-                <h1>INFO LUDA</h1>
+            <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <img src = "images/nastya_smooth.jpg" class = "img-responsive img-circle center-block">
+                <p class = "lead text-justify">
+                    информационный текст текст текст текст
+                    текст текст. текст етксткт. текст текст.
+                    текст етксткт. текст текст. текст етксткт.
+                    текст етксткт. текст текст.
+                </p>
+                <a href = "views/anastasiya" role = "button" class = "btn btn-block btn-primary btn-lg">Блог</a>
+                <!-- MODAL TRIGGER FOR NASTYA'S CONTACTS -->
+                <button type = "button" class = "btn btn-block btn-primary btn-lg" data-toggle = "modal" data-target = "#contacts_luda">контакты</button>
+                <!-- MODAL TRIGGER FOR MAILTO -->
+                <button type = "button" class = "btn btn-block btn-success btn-lg" data-toggle = "modal" data-target = "#mailto">оставить сообщение</button>
+            </div>
             </div>
         </div>
     </section>
     
     <section class = "container">
         <div class = "row">
-            <div class = "col-md-12">
-                <h1>HELP DIRECTIONS</h1>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "row">
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-3 col-sm-6">
+                <div class = "panel panel-info">
+                    <div class = "panel-heading">
+                        <h3>Проблемы одиночества</h3>
+                    </div>
+                    <div class = "panel-body">
+                        <p class = "text-justify">
+                            Каждый человек может столкнуться с
+                            проблемой оиночества. Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            Каждый человек может столкнуться с
+                            проблемой оиночества.
+                            
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -56,6 +270,86 @@
         </div>
     
     </footer>
-        
+    
+    
+    
+    <!-- MODAL CONTACTS NASTYA -->
+    <div id = "contacts_nastya" class = "modal fade" role = "dialog">
+        <div class = "modal-dialog">
+            <div class = "modal_content">
+                <div class = "modal-header">
+                    <button type = "button" class = "close" data-dismiss = "modal">&times;</button> 
+                    <h4 class = "modal-title">Контакты Анастасии</h4>
+                </div>
+                <div class = "modal-body">
+                    <adress>
+                        <abbr title = "Мобильный номер"></abbr>+7 (123) 123-3243 <br>
+                    </adress>
+                    <br>
+                    <adress>
+                        <strong>Анастасия Матенко</strong><br>
+                        <a href = "mailto:#">matenko@mail.ru</a>
+                    </adress>
+                    <adress>
+                        <h3>skype:</h3>
+                    </adress>
+                </div>
+                <div class = "modal-footer">
+                    <button type = "button" class = "btn btn-default" data-dismiss = "modal">Закрыть</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- END OF MODAL -->
+    <!-- MODAL CONTACTS LUDA -->
+    <div id = "contacts_luda" class = "modal fade" role = "dialog">
+        <div class = "modal-dialog">
+            <div class = "modal_content">
+                <div class = "modal-header">
+                    <button type = "button" class = "close" data-dismiss = "modal">&times;</button> 
+                    <h4 class = "modal-title">Контакты Людмилы</h4>
+                </div>
+                <div class = "modal-body">
+                    <adress>
+                        <abbr title = "Мобильный номер"></abbr>+7 (123) 123-3243 <br>
+                    </adress>
+                    <br>
+                    <adress>
+                        <strong>Людмила Логвенкова</strong><br>
+                        <a href = "mailto:#">logvenkova@mail.ru</a>
+                    </adress>
+                    <adress>
+                        <h3>skype: </h3>
+                    </adress>
+                </div>
+                <div class = "modal-footer">
+                    <button type = "button" class = "btn btn-default" data-dismiss = "modal">Закрыть</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- END OF MODAL -->
+    <!-- MODAL MAILTO -->
+    <div id = "mailto" class = "modal fade" role = "dialog">
+        <div class = "modal-dialog">
+            <div class = "modal_content">
+                <div class = "modal-header">
+                    <button type = "button" class = "close" data-dismiss = "modal">&times;</button> 
+                    <h4 class = "modal-title">отправить сообщение Анастасии</h4>
+                </div>
+                <div class = "modal-body">
+                    <h1>MAILTO FORM WITH DB STORE</h1>
+                </div>
+                <div class = "modal-footer">
+                    <button type = "button" class = "btn btn-default" data-dismiss = "modal">Закрыть</button>
+                    <button type = "button" class = "btn btn-success" data-dismiss = "modal" name = "mailto_nastya">отправить</button> 
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
