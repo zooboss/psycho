03b417ce16b6d61b8a178affc4332298e4a702ab
zooboss <sandrozz@yandex.ru>
2016-05-29 13:55:09 +0300

    Minor

--- veiws/ajax_mailto_default.php
+++ veiws/ajax_mailto_default.php
@@ -2,7 +2,7 @@
             <div class = "modal_content">
                 <div class = "modal-header">
                     <button type = "button" class = "close" data-dismiss = "modal">&times;</button> 
-                    <h4 class = "modal-title">отправить сообщение Анастасии</h4>
+                    <h4 class = "modal-title">отправить сообщение</h4>
                 </div>
                 <div class = "modal-body">
                     <form method = "post" action="">
