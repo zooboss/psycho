44f393003638fbab9c6902551c27562f38428cc5
zooboss <sandrozz@yandex.ru>
2016-05-29 13:53:06 +0300

    same

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 //require_once(dirname(__FILE__) . "/models/database.php");
 require_once(dirname(__FILE__) . "/models/functions.php");
 
-$link = db_connect();
+//$link = db_connect();
 
 include(dirname(__FILE__) . "/veiws/main_page.php");
