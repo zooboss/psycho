870d901e0b1016e2b458f3cce1268305b14326b0
zooboss <sandrozz@yandex.ru>
2016-06-02 10:07:23 +0300

    Hello, World working on reactJS

--- veiws/main_page.php
+++ veiws/main_page.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Psychology site</title>
     <link rel="stylesheet" href="libraries/css/bootstrap.css">
+    <link rel="stylesheet" href="css/custom.css">
     <script rel = "text/javascript" src = "libraries/js/jquery-2.2.4.js"></script>
     <script rel="text/javascript" src="libraries/js/bootstrap.js"></script>
+    <script rel = "text/javascript" src = "js/ajax.js"></script>
+    <script src="https://fb.me/react-15.1.0.js"></script>
+    <script src="https://fb.me/react-dom-15.1.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
     <script rel = "text/javascript" src = "js/main.js"></script>
     <script rel = "text/javascript" src = "js/ajax.js"></script>
-    <link rel="stylesheet" href="css/custom.css">
 </head>
 <body>
     <a href = "">
---
+++ veiws/reactTrain.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HMTL>
+<html lang = "ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Psychology site</title>
+    <link rel="stylesheet" href="../libraries/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/custom.css">
+    <script rel = "text/javascript" src = "../libraries/js/jquery-2.2.4.js"></script>
+    <script rel="text/javascript" src="../libraries/js/bootstrap.js"></script>
+    <script rel = "text/javascript" src = "../js/ajax.js"></script>
+    <script src="https://fb.me/react-15.1.0.js"></script>
+    <script src="https://fb.me/react-dom-15.1.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
+    <script rel = "text/javascript" src = "../js/main.js"></script>
+    <script rel = "text/javascript" src = "../js/ajax.js"></script>
+</head>
+
+<body>
+    
+    <div id="content">
+    
+    </div>
+    
+    <script type="text/babel">
+        var HelloWorld = React.createClass({
+            render: function()
+            {
+                return <h1> Hello, World! </h1>;
+            }
+        });
+        ReactDOM.render(
+            <HelloWorld />,
+            document.getElementById("content")
+        );
+    
+    
+    
+    </script>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</body>
+</html>
