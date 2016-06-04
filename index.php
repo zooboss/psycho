2d30460df032d55422487ddbe0ae0afdb8c48e6e
zooboss <sandrozz@yandex.ru>
2016-06-04 14:26:32 +0300

    Новый урок на реакте

--- index.php
+++ index.php
@@ -6,3 +6,5 @@ require_once(dirname(__FILE__) . "/models/functions.php");
 //$link = db_connect();
 
 include(dirname(__FILE__) . "/veiws/main_page.php");
+
+?>
--- veiws/reactTrain.php
+++ veiws/reactTrain.php
@@ -1,130 +1,118 @@
 <!DOCTYPE HMTL>
-<html lang = "ru">
+<html lang="ru">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Psychology site</title>
     <link rel="stylesheet" href="../libraries/css/bootstrap.css">
     <link rel="stylesheet" href="../css/custom.css">
-    <script rel = "text/javascript" src = "../libraries/js/jquery-2.2.4.js"></script>
+    <script rel="text/javascript" src="../libraries/js/jquery-2.2.4.js"></script>
     <script rel="text/javascript" src="../libraries/js/bootstrap.js"></script>
-    <script rel = "text/javascript" src = "../js/ajax.js"></script>
+    <script rel="text/javascript" src="../js/ajax.js"></script>
     <script src="https://fb.me/react-15.1.0.js"></script>
     <script src="https://fb.me/react-dom-15.1.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
-    <script rel = "text/javascript" src = "../js/main.js"></script>
-    <script rel = "text/javascript" src = "../js/ajax.js"></script>
+    <script rel="text/javascript" src="../js/main.js"></script>
+    <script rel="text/javascript" src="../js/ajax.js"></script>
 </head>
 
 <body>
-    
-    <div id="content">
-    
-    </div>
-    
-    <script type="text/babel">
-       
-       var ARTICLES = [
-           {
+
+<div id="content">
+
+</div>
+<!-- Search utility on reactJS -->
+<!--
+<script type="text/babel">
+
+    var ARTICLES = [
+        {
             id: 0,
             title: "title0",
             text: "text0"
-        },
-         {
-            id: 1,
-            title: "title1",
-            text: "text1"
-        },
-         {
+        }, {
             id: 2,
             title: "title2",
             text: "text2"
+        },
+        {
+            id: 1,
+            title: "title1",
+            text: "text1"
         }
-       ];
-       
-        var Article = React.createClass({
-            render: function()
-            {
-                return (<li> 
-                            <div>{this.props.title}</div>
-                            <div>{this.props.text}</div> 
-                        </li>);
-            }
-        });
-        
-        var ArticleList = React.createClass({
-            getInitialState: function() {
-                return {
-                    displayedArticles: ARTICLES
-                };
-            },
-                
-            handleSearch: function(event) {
-                console.log(event.target.value);
-                var searchQuery = event.target.value.toLowerCase();
-                var displayedArticles = ARTICLES.filter(function(el){
-                    var searchValue = el.text.toLowerCase();
-                    return searchValue.indexOf(searchQuery) !== -1;
-                });
-                console.log(displayedArticles);
-                this.setState({
-                    displayedArticles: displayedArticles
-                });
-            },
-            
-            render: function(){
-                return (
+    ];
+
+
+    var Article = React.createClass({
+        render: function () {
+            return (<li>
+                <div>{this.props.title}</div>
+                <div>{this.props.text}</div>
+            </li>);
+        }
+    });
+
+    var ArticleList = React.createClass({
+        getInitialState: function () {
+            return {
+                displayedArticles: ARTICLES
+            };
+        },
+
+        handleSearch: function (event) {
+            console.log(event.target.value);
+            var searchQuery = event.target.value.toLowerCase();
+            var displayedArticles = ARTICLES.filter(function (el) {
+                var searchValue = el.text.toLowerCase();
+                return searchValue.indexOf(searchQuery) !== -1;
+            });
+            console.log(displayedArticles);
+            this.setState({
+                displayedArticles: displayedArticles
+            });
+        },
+
+        render: function () {
+            return (
                     <div>
-                        <input type = "text" onChange = {this.handleSearch}/>
+                        <input type="text" onChange={this.handleSearch}/>
                         <ul>
-                           {
-                                this.state.displayedArticles.map(function(el){
-                                    return <Article 
-                                                key   = {el.id} 
-                                                title = {el.title}
-                                                text  = {el.text} />;
-                                    
+                            {
+                                this.state.displayedArticles.map(function (el) {
+                                    return <Article
+                                            key={el.id}
+                                            title={el.title}
+                                            text={el.text}/>;
 
 
                                 })
                             }
                         </ul>
                     </div>
-                )
-            }
-        });
-        
-        ReactDOM.render(
+            )
+        }
+    });
+
+    ReactDOM.render(
             <ArticleList />,
             document.getElementById("content")
-        );
-    
-    
-    
-    </script>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    );
+
+
+</script>
+-->
+
+<!-- Another train with reactJS -->
+<script type="text/babel">
+    ReactDOM.render(
+            React.createElement('h1', null, 'Hello, World'),
+            document.getElementById('content')
+    )
+
+
+</script>
+
+
+
 </body>
 </html>
